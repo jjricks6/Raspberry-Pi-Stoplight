5abff06769a47b2b0f071173a4ed51a97ee562b7
jjricks6 <jjricks1@gmail.com>
2021-09-16 19:09:59 -0600

    Comments in code

--- README.md
+++ README.md
@@ -7,17 +7,71 @@ The goal of this project is to create a Raspberry Pi "stoplight" that can be con
 * Raspberry Pi (I will be using a Pi 4b)
 * 3 LED lights (Green, Yellow, and Red)
 * A breadboard
-* Some wires
+* 4 wires
+* 3 resistors
 
 ## Design
 Our stoplight should be able to work in both Manual and Automatic modes. In Manual mode, a user is able to select any light for the stoplight to switch to including an "off" state. In Automatic mode a timer starts in the Green state and moves between Green, Yellow, and Red states automatically. The user should be able to switch between these two modes at will using the web interface.
 
-State Diagram
+### State Diagram
+![](/images/StoplightDiagram.png)
 
 ## Creating Our Website
+There are many ways to create a webserver in Python, but for the sake of this project we will be using http.python.
 
-## Wiring Our Pi
+Create a new folder to work out and create a new python file. I named mine webserver.py, but feel free to name it anything you like.
 
+First we need to import http.python
+
+```python
+from http.server import BaseHTTPRequestHandler, HTTPServer
+```
+
+Define your host name and host port. Your host name is the ip address of your Pi, and the port is 8000.
+
+```python
+host_name = '127.0.1.1'    # Change this to your Raspberry Pi IP address
+host_port = 8000
+```
+
+Next we need to define a custom server class using the BaseHTTPRequestHandler we imported. Inside the class we must define 2 functions for the server to work: do_HEAD and do_GET
+
+```python
+def do_HEAD(self):
+    self.send_response(200)
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+
+def do_GET(self):
+    html = '''
+        <html>
+        <body style="width:960px; margin: 20px auto;">
+        <h1>Raspberry Pi Stoplight</h1>
+        <p>Turn LED:
+            <a href="/Green">Green</a>
+            <a href="/Yellow">Yellow</a>
+            <a href="/Red">Red</a>
+            <a href="/Off">Off</a>
+            <a href="/Auto">Automatic</a>
+        </p>
+        </body>
+        </html>
+    '''
+    self.do_HEAD()
+```
+## Wiring the Pi
+
+Ground -> Board Pin 9
+
+Green  -> GPIO Pin 17
+
+Yellow -> GPIO Pin 27
+
+Red    -> GPIO Pin 22
+
+Be sure to add a resistor before the current exits to prevent LED burnout.
+
+![](/images/wiring.jpeg)
 ## Programming the Stoplight
 
 ## Sources
--- webserver.py
+++ webserver.py
@@ -1,5 +1,4 @@
 import RPi.GPIO as GPIO
-import os
 import time
 from threading import Thread
 from http.server import BaseHTTPRequestHandler, HTTPServer
@@ -8,43 +7,49 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 host_name = '127.0.1.1'    # Change this to your Raspberry Pi IP address
 host_port = 8000
 
+# Mode is a global variable which will help us switch between automatic and manual modes
 mode = "Auto"
+
+
+# Custom Server class built using the base http handler
 class MyServer(BaseHTTPRequestHandler):
-    
+
+    # The automatic stoplight function is started in a thread
+    # Exits only when the mode has changed
     def auto(self):
         global mode
         while mode == 'Auto':
             GPIO.output(17, GPIO.HIGH)
             GPIO.output(27, GPIO.LOW)
             GPIO.output(22, GPIO.LOW)
-            status='Green LED is On'
 
             time.sleep(5)
-            if mode !='Auto':
+            if mode != 'Auto':
                 return
-            
+
             GPIO.output(17, GPIO.LOW)
             GPIO.output(27, GPIO.HIGH)
-            status='Yellow LED is On'
-            
+
             time.sleep(2)
-            if mode !='Auto':
+            if mode != 'Auto':
                 return
-            
+
             GPIO.output(27, GPIO.LOW)
             GPIO.output(22, GPIO.HIGH)
-            status='Red LED is On'
-            
+
             time.sleep(5)
-            if mode !='Auto':
+            if mode != 'Auto':
                 return
-    
+
+    # Defines the headers
     def do_HEAD(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
+    # Defines what happens when the page loads
     def do_GET(self):
+        # Simple html page with hyperlinks as buttons
         html = '''
             <html>
             <body style="width:960px; margin: 20px auto;">
@@ -60,48 +65,54 @@ class MyServer(BaseHTTPRequestHandler):
             </html>
         '''
         self.do_HEAD()
+
+        # Set mode to be global
         global mode
-        if self.path=='/':
+
+        # Setup the GPIO pins on the index page
+        if self.path == '/':
             GPIO.setmode(GPIO.BCM)
             GPIO.setwarnings(False)
             GPIO.setup(17, GPIO.OUT)
             GPIO.setup(27, GPIO.OUT)
             GPIO.setup(22, GPIO.OUT)
-        elif self.path=='/Green':
+        elif self.path == '/Green':
             mode = "Man"
             GPIO.output(17, GPIO.HIGH)
             GPIO.output(27, GPIO.LOW)
             GPIO.output(22, GPIO.LOW)
             print("hello")
-        elif self.path=='/Yellow':
+        elif self.path == '/Yellow':
             mode = "Man"
             GPIO.output(17, GPIO.LOW)
             GPIO.output(27, GPIO.HIGH)
             GPIO.output(22, GPIO.LOW)
-        elif self.path=='/Red':
+        elif self.path == '/Red':
             mode = "Man"
             GPIO.output(17, GPIO.LOW)
             GPIO.output(27, GPIO.LOW)
             GPIO.output(22, GPIO.HIGH)
-        elif self.path=='/Off':
+        elif self.path == '/Off':
             mode = "Man"
             GPIO.output(17, GPIO.LOW)
             GPIO.output(27, GPIO.LOW)
             GPIO.output(22, GPIO.LOW)
-        elif self.path=='/Auto':
+        elif self.path == '/Auto':
             mode = "Auto"
+
+            # Creates a thread so that you can exit the auto state
             auto = Thread(target=self.auto)
             auto.start()
-            print("testy")
+
+        # After the GPIO action, rewrite the page
         self.wfile.write(html.format().encode("utf-8"))
-        
-    
-        
-        
+
+
+# Run the server when the script starts
 if __name__ == '__main__':
     http_server = HTTPServer((host_name, host_port), MyServer)
     print("Server Starts - %s:%s" % (host_name, host_port))
-    
+
     try:
         http_server.serve_forever()
     except KeyboardInterrupt:
